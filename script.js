80f112f6d5df246e64d31a84319521935bb870a0
Naveen <snaveen1234vpm@gmail.com>
2024-08-19 11:16:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function nextpage(){
+    window.location.href="yes.html";
+}
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const noBtn = document.querySelector('.no-btn');
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
+
+noBtn.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+    noBtn.style.left = i + 'px';
+    noBtn.style.top = j + 'px';
+});
